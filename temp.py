d327ac4863342b7cc13d309077c363d861296d13
aspectum <drogpe@gmail.com>
2019-07-11 15:09:10 -0300

    Fixing mistakes

--- temp.py
+++ temp.py
@@ -1,20 +1,23 @@
 ## To save after trained offline
 from qLearning_offline import QLearning_offline
-def save_offline(self):
+
+
+def save_offline(obj):
     with open('offline_learn.txt', 'w') as f:
-        for i in self.learn.q_value_table:
-            for j in self.learn.q_value_table[i]:
-                value = self.learn.q_value_table[i][j].get_val_at_t(0)
+        for i in obj.learn.q_value_table:
+            for j in obj.learn.q_value_table[i]:
+                value = obj.learn.q_value_table[i][j].get_val_at_t(0)
                 line = str(i) + ', ' + str(j) + ', ' + str(value) + '\n'
                 f.write(line)   
 
+
 ## To load to train online (use this values as ininial)
-def load_online(self):
-    self.learn_offline_initial_conditions = QLearning_offline(self)
+def load_online(obj):
+    obj.learn_offline_initial_conditions = QLearning_offline(obj)
     with open('offline_learn.txt', 'r') as f:
         for line in f:
             words = line.split(',')
             x = words[0]
             y = words[1]
             value = words[2]
-            self.learn_offline.q_value_table[x][y].val = value
+            obj.learn_offline.q_value_table[x][y].val = value
