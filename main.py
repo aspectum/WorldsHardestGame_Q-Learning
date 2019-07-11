e5729c661dd9b97e36b18f4b60746fbbb7f55175
Vitor <vitorhpgomes@gmail.com>
2019-07-11 11:18:54 -0300

    Added colision flag for training without enemies

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import time
 
 start = time.time()
 
-game = Game(level=4, watch=False, watch_periodic=False, replay=True)
+game = Game(level=0, watch=True, watch_periodic=False, replay=False)
 # watch overrides watch_periodic
 
 # General parameters
 game.player_max_moves = 100
-game.player_max_max_moves = 300
-game.player_vel = 10
+game.player_max_max_moves = 1000
+game.player_vel = 5
 game.player_moves_step = 5
 game.player_moves_interval = 5
 game.eps_decrease_interval = 40
 game.iteration_print_interval = 50
-game.max_iterations = 5000
+game.max_iterations = 15000
 
 # Q-Learning parameters
 game.learn.eps = 0.8
@@ -22,9 +22,12 @@ game.learn.lr = 0.4
 game.learn.gamma = 0.9
 
 # Watcher parameters
-game.watcher_clock_flag = True
+game.watcher_clock_flag = False
 game.watch_period = 50
-game.watch_duration = 3
+game.watch_duration = 1
+
+# Colision On/Off
+game.colision = False
 
 game.start()
 
