b6d2daa6896c3a51b5d98362db4a0f40e6198f59
aspectum <drogpe@gmail.com>
2019-07-11 10:34:52 -0300

    Changed to accept custom replay filenames

--- play_replay.py
+++ play_replay.py
@@ -1,11 +1,23 @@
 from game import Game
 from watch import watcher
+import sys
 import pickle
+import os
 
 game = Game(level=0, watch=True, watch_periodic=False, replay=False)
 game.watcher_clock_flag = True
 
-with open('replay.p', 'rb') as f:
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = 'replay.p'
+
+if not os.path.isfile(filename):
+    print("ERROR: the file '", filename, "' doesn't exist")
+    sys.exit()
+
+
+with open(filename, 'rb') as f:
     final_state = pickle.load(f)
-w = watcher(game, game.watcher_clock_flag)
-w.replay(final_state)
+    w = watcher(game, game.watcher_clock_flag)
+    w.replay(final_state)
