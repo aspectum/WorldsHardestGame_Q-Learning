3d0cbd7cfb2e7530d351ce36dc821f772e5aab74
Vitor <vitorhpgomes@gmail.com>
2019-07-11 14:37:26 -0300

    Offline learning

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 start = time.time()
 
-game = Game(level=0, watch=True, watch_periodic=False, replay=False)
+game = Game(level=5, watch=True, watch_periodic=False, replay=False)
 # watch overrides watch_periodic
 
 # General parameters
@@ -12,14 +12,14 @@ game.player_max_max_moves = 1000
 game.player_vel = 5
 game.player_moves_step = 5
 game.player_moves_interval = 5
-game.eps_decrease_interval = 40
+game.eps_decrease_interval = 100
 game.iteration_print_interval = 50
 game.max_iterations = 15000
 
 # Q-Learning parameters
 game.learn.eps = 0.8
 game.learn.lr = 0.4
-game.learn.gamma = 0.9
+game.learn.gamma = 0.7
 
 # Watcher parameters
 game.watcher_clock_flag = False
@@ -27,7 +27,7 @@ game.watch_period = 50
 game.watch_duration = 1
 
 # Colision On/Off
-game.colision = False
+game.colision = True
 
 game.start()
 
